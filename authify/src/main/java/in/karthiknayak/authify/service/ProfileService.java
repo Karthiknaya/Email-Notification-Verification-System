c6133ece77e09c18e9826902392b2037742a9e6c
KarthikNayakEmotorad <karthik.nayak@emotorad.in>
2025-04-30 10:37:34 +0530

    first commit

---
+++ authify/src/main/java/in/karthiknayak/authify/service/ProfileService.java
@@ -0,0 +1,19 @@
+package in.karthiknayak.authify.service;
+
+import in.karthiknayak.authify.io.ProfileRequest;
+import in.karthiknayak.authify.io.ProfileResponse;
+
+public interface ProfileService {
+
+    ProfileResponse createProfile(ProfileRequest request);
+
+    ProfileResponse getProfile(String email);
+
+    void sendResetOtp(String email);
+
+    void resetPassword(String email, String otp, String newPassword);
+
+    void sendOtp(String email);
+
+    void verifyOtp(String email, String otp);
+}
